12c342d01954f8d2bf2b7b17c0dc7021edb24414
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2018-09-23 14:46:04 -0400

    Make source & target parametrized to flexibly allow similar arguments for a restore.

--- backup.sh
+++ backup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -
 
-folder=$1
-echo "Host Folder for Backup: ${folder}"
-path=$(basename $folder)
+source_folder=$1
+target_folder=$2
+echo "Source Folder: ${source_folder}"
+echo "Target Folder: ${target_folder}"
+
+path=$(basename ${source_folder})
 
 echo "Extracted base folder: ${path}"
 
-mkdir -p ./backup/${path}
-cp -r ${folder}/* ./backup/${path}/
+mkdir -p ${target_folder}/${path}
+cp -r ${source_folder}/* ${target_folder}/${path}/
 
-ls ./backup/${path}
+ls ${target_folder}/${path}
